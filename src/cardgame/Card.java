8d03fcdda4f36a2969177df9f3f869ac1006a089
arshd <arshd@HP-Pavilion>
2022-07-01 02:11:52 -0400

    Group#2

---
+++ src/cardgame/Card.java
@@ -0,0 +1,55 @@
+
+package cardgame;
+
+/**
+ * Group#2
+ * @author Arshdeep Singh Khosa 
+ * @author Nishant Choudary (Leader)
+ * @author Sakshi Sakshi
+ * @author Sukhvir Singh
+ */
+public class Card {
+    
+    //Code Starts here
+
+    //  Creating a special Class for Colours.
+    public enum BellBott{
+    Red,Yellow,Green,Blue
+    }
+
+    //  Creating a special Class for Cards.
+    public enum merit{
+        One, TWO, THREE,FOUR,FIVE,SIX,
+        SEVEN,EIGHT,NINE,TEN,Skip,Reverse,
+        Draw_two,Draw_four, wildcard
+    }
+    
+    //Creating private classes
+    private final BellBott bellbott;
+    private final merit merit;
+
+    //Creating a public classs for card
+    public Card(BellBott bellbott,
+                merit merit)
+    
+
+    //using this for constructor
+    {
+        this.bellbott=bellbott;
+        this.merit=merit;
+    }
+   
+    //Creating private classes
+    public BellBott getBellBott() {
+        //returing values for bellbott
+        return bellbott;
+    }
+
+    //Creating private classes
+    public merit getmerit() {
+         //returing values for merit
+        return merit;
+    }
+}
+
+//code ends here
---
+++ src/cardgame/GamePlayer.java
@@ -0,0 +1,32 @@
+package cardgame;
+
+/**
+ * Group#2
+ * @author Arshdeep Singh Khosa 
+ * @author Nishant Choudary (Leader)
+ * @author Sakshi Sakshi
+ * @author Sukhvir Singh
+ */
+
+ //creating a new class
+public class GamePlayer {
+
+    //creating a void
+    public static void main(String[] args) {
+
+        //Scanner class
+        CardHand PEEK=new CardHand();
+        //generating numbers/Values
+        PEEK.generateHand();
+        
+        //for loop 
+        for(Card ZEE:PEEK.Patte)
+        {
+            //Printing Statement
+            System.out.println(ZEE.getmerit()+" of "+ZEE.getBellBott());
+        }
+    }
+
+}
+
+//code ends here
